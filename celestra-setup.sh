a8cfa3cc81d0705114d1b48f9e30fd2765140649
sysad <sysad@celestra11.(none)>
2013-02-14 13:16:28 +0100

    added working config-script

---
+++ celestra-setup.sh
@@ -0,0 +1,42 @@
+# Download RVM
+echo "Downloading rvm..."
+curl -L https://get.rvm.io | bash -s stable > /dev/null
+
+# Setup the proper path for use on Ubuntu
+echo "Setting up proper env"
+cp .bashrc tmp_bash_rc
+cat tmp_bash_rc .bash_profile > .bashrc
+rm tmp_bash_rc 
+
+# Download and install the proper Ruby
+echo "Downloading and installing Ruby"
+rvm install 1.9.2-p180
+source .bashrc
+rvm alias create default 1.9.2-p180
+
+echo "Installing Bundler"
+gem install bundler
+source .bashrc
+
+# Download Staffz serialport
+git clone git://github.com/staffz/em-serialport.git
+cd em-serialport
+gem build em-serialport.gemspec
+gem install em-serialport-staffz
+cd ..
+
+# Download Celestra Codebase 
+echo "Downloading Celestra codebase..."
+git clone git@github.com:buffpojken/Celestra.git
+
+# Installing dependencies
+echo "Installing Celestra dependencies" 
+cd Celestra/Helm
+bundle install
+cd ../..
+
+# Download proper startscreen
+cd Downloads
+wget http://cl.ly/image/0t1N1t2Z0U41/Start2.png
+
+
