b580215e3c557f1fa3b2ca982a32197fa652fc4a
buffpojken <daniel@sykewarrior.com>
2013-02-27 10:45:48 +0100

    added torpedo-for conf

--- console-startup.sh
+++ console-startup.sh
@@ -1,5 +1,5 @@
 xdotool mousemove 4000 4000
-cd /home/sysad/Celestra/Sensor/Dradis
-/home/sysad/.rvm/rubies/ruby-1.9.2-p180/bin/ruby server -s ninja > /home/sysad/rubylog &
+cd /home/sysad/Celestra/Weapons/Torpedo
+/home/sysad/.rvm/rubies/ruby-1.9.2-p180/bin/ruby server -s ninja -c celestra -d for -r > /home/sysad/rubylog &
 sleep 10
-chromium-browser --kiosk http://0.0.0.0:4001 &
+chromium-browser --kiosk http://0.0.0.0:4000 &
