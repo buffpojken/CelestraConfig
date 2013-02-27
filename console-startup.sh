284a9958e8cc41bf63fa7178fa3f1c0c059c50f6
buffpojken <daniel@sykewarrior.com>
2013-02-27 10:42:51 +0100

    added helm config

--- console-startup.sh
+++ console-startup.sh
@@ -1,5 +1,5 @@
 xdotool mousemove 4000 4000
-cd /home/sysad/Celestra/Sensor/Dradis
+cd /home/sysad/Celestra/Helm
 /home/sysad/.rvm/rubies/ruby-1.9.2-p180/bin/ruby server -s ninja > /home/sysad/rubylog &
 sleep 10
-chromium-browser --kiosk http://0.0.0.0:4001 &
+chromium-browser --kiosk http://0.0.0.0:4002 &
