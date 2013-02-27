e1cac9f43804b7d2c8af2f70b7e8be4a9eab45c8
buffpojken <daniel@sykewarrior.com>
2013-02-27 10:46:37 +0100

    added dc config

--- console-startup.sh
+++ console-startup.sh
@@ -1,5 +1,5 @@
 xdotool mousemove 4000 4000
-cd /home/sysad/Celestra/Sensor/Dradis
+cd /home/sysad/Celestra/DamageControl
 /home/sysad/.rvm/rubies/ruby-1.9.2-p180/bin/ruby server -s ninja > /home/sysad/rubylog &
 sleep 10
-chromium-browser --kiosk http://0.0.0.0:4001 &
+chromium-browser --kiosk http://0.0.0.0:4010 &
