a0243af872dd1d0bd36ef1b4034b2335543b9c13
buffpojken <daniel@sykewarrior.com>
2013-02-27 11:42:59 +0100

    tweaks

--- console-startup.sh
+++ console-startup.sh
@@ -1,5 +1,5 @@
 xdotool mousemove 4000 4000
 cd /home/sysad/Celestra/Coms/Client
-/home/sysad/.rvm/rubies/ruby-1.9.2-p180/bin/ruby server -s ninja > /home/sysad/rubylog &
+/home/sysad/.rvm/rubies/ruby-1.9.2-p180/bin/ruby server.rb -s ninja > /home/sysad/rubylog &
 sleep 10
 chromium-browser --kiosk http://0.0.0.0:4000 &
