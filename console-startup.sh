de2cf8f22757e0d2aad1228392a00ddaa4bea712
buffpojken <daniel@sykewarrior.com>
2013-02-28 17:21:57 +0100

    added reactor

--- console-startup.sh
+++ console-startup.sh
@@ -2,4 +2,4 @@ xdotool mousemove 4000 4000
 cd /home/sysad/Celestra/Reactor/Client
 /home/sysad/.rvm/rubies/ruby-1.9.2-p180/bin/ruby server.rb -s ninja > /home/sysad/rubylog &
 sleep 10
-chromium-browser --kiosk http://0.0.0.0:4006 &
+chromium-browser --incognito --kiosk http://0.0.0.0:4006 &
