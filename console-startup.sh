538997c656ded2492aaf80957fa436163c9bb699
buffpojken <daniel@sykewarrior.com>
2013-02-28 17:22:33 +0100

    added incog

--- console-startup.sh
+++ console-startup.sh
@@ -2,4 +2,4 @@ xdotool mousemove 4000 4000
 cd /home/sysad/Celestra/Weapons/Torpedo
 /home/sysad/.rvm/rubies/ruby-1.9.2-p180/bin/ruby server.rb -s ninja -c celestra -d for -r > /home/sysad/rubylog &
 sleep 10
-chromium-browser --kiosk http://0.0.0.0:4000 &
+chromium-browser --incognito --kiosk http://0.0.0.0:4000 &
