a23d66d6991eb0fc18bd0f93bf9b2b9d5f62a2f2
buffpojken <daniel@sykewarrior.com>
2013-02-28 17:21:23 +0100

    added incog

--- console-startup.sh
+++ console-startup.sh
@@ -2,4 +2,4 @@ xdotool mousemove 4000 4000
 cd /home/sysad/Celestra/Sensor/Hades
 /home/sysad/.rvm/rubies/ruby-1.9.2-p180/bin/ruby server.rb -s ninja > /home/sysad/rubylog &
 sleep 10
-chromium-browser --kiosk http://0.0.0.0:4005 &
+chromium-browser --incognito --kiosk http://0.0.0.0:4005 &
